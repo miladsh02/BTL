6445e67cd9b109e6bf02f509ff6cc4bfbcd12bd0
Milad <milad.shafiei81@gmail.com>
2023-05-23 03:31:50 +0330

    Refactor:seperate UserRoleDetailDto class

---
+++ Domain/DtoModels/UserRoleDetailDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DtoModels
+{
+    public class UserRoleDetailDto
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string UniversityId { get; set; } = null!;
+        public string NationalCode { get; set; } = null!;
+        public List<string>? UserRoleName { get; set; }
+        public List<string>? RoleNameList { get; set; }
+    }
+}
